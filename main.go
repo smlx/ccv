7428b3c12f03388cff2cddd2664505fc448771a1
Scott Leggett <scott@sl.id.au>
2021-02-08 22:51:48 +0800

    chore: refactor in preparation for tests

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/Masterminds/semver/v3"
@@ -12,20 +11,17 @@ import (
 	"go.uber.org/zap"
 )
 
-func main() {
-	// init logger
-	log, err := zap.NewProduction()
-	if err != nil {
-		panic(err)
-	}
+// nextVersion returns a string containing the next version based on the state
+// of the git repository in path.
+func nextVersion(log *zap.Logger, path string) (string, error) {
 	// open repository
-	r, err := git.PlainOpenWithOptions(".", &git.PlainOpenOptions{DetectDotGit: true})
+	r, err := git.PlainOpenWithOptions(path, &git.PlainOpenOptions{DetectDotGit: true})
 	if err != nil {
-		log.Fatal("couldn't open git repository", zap.Error(err))
+		return "", fmt.Errorf("couldn't open git repository: %w", err)
 	}
 	tags, err := r.Tags()
 	if err != nil {
-		log.Fatal("couldn't get tags", zap.Error(err))
+		return "", fmt.Errorf("couldn't get tags: %w", err)
 	}
 	// map tags to commit hashes
 	tagRefs := map[string]string{}
@@ -34,34 +30,19 @@ func main() {
 		return nil
 	})
 	if err != nil {
-		log.Fatal("couldn't iterate tags", zap.Error(err))
+		return "", fmt.Errorf("couldn't iterate tags: %w", err)
 	}
 	// walk commit hashes back from HEAD
 	commits, err := r.Log(&git.LogOptions{})
 	if err != nil {
-		log.Fatal("couldn't get commits", zap.Error(err))
+		return "", fmt.Errorf("couldn't get commits: %w", err)
 	}
 	var major, minor, patch bool
-	commits.ForEach(func(c *object.Commit) error {
-		if tag := tagRefs[c.Hash.String()]; tag != "" {
-			// found a tag
-			t, err := semver.NewVersion(tag)
-			if err != nil {
-				log.Fatal("couldn't parse tag", zap.Error(err), zap.String("tag", tag))
-			}
-			var v semver.Version
-			switch {
-			case major:
-				v = t.IncMajor()
-			case minor:
-				v = t.IncMinor()
-			case patch:
-				v = t.IncPatch()
-			default:
-				v = *t
-			}
-			fmt.Printf("%s%s\n", "v", v.String())
-			os.Exit(0)
+	var stopIter error = fmt.Errorf("stop commit iteration")
+	var latestTag string
+	err = commits.ForEach(func(c *object.Commit) error {
+		if latestTag = tagRefs[c.Hash.String()]; latestTag != "" {
+			return stopIter
 		}
 		// analyze commit message
 		if strings.HasPrefix(c.Message, "fix: ") {
@@ -75,4 +56,36 @@ func main() {
 		}
 		return nil
 	})
+	if (err != nil && err != stopIter) || latestTag == "" {
+		return "", fmt.Errorf("couldn't determine latest tag: %w", err)
+	}
+	// found a tag: parse, increment, and return.
+	latestVersion, err := semver.NewVersion(latestTag)
+	if err != nil {
+		return "", fmt.Errorf(`couldn't parse tag "%v": %w`, latestTag, err)
+	}
+	var newVersion semver.Version
+	switch {
+	case major:
+		newVersion = latestVersion.IncMajor()
+	case minor:
+		newVersion = latestVersion.IncMinor()
+	case patch:
+		newVersion = latestVersion.IncPatch()
+	default:
+		newVersion = *latestVersion
+	}
+	return fmt.Sprintf("%s%s", "v", newVersion.String()), nil
+}
+
+func main() {
+	log, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	next, err := nextVersion(log, `.`)
+	if err != nil {
+		log.Fatal("couldn't get next version", zap.Error(err))
+	}
+	fmt.Println(next)
 }
