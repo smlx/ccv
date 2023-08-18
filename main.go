0274494bdb359203bfc1b36a0ac5730d03bd84cd
Scott Leggett <scott@sl.id.au>
2023-08-18 08:30:50 +0800

    feat: export NextVersion()
    This makes the package importable.

--- main.go
+++ main.go
@@ -59,9 +59,9 @@ func walkCommits(r *git.Repository, tagRefs map[string]string, order git.LogOrde
 	return latestVersion, major, minor, patch, nil
 }
 
-// nextVersion returns a string containing the next version based on the state
+// NextVersion returns a string containing the next version based on the state
 // of the git repository in path.
-func nextVersion(path string) (string, error) {
+func NextVersion(path string) (string, error) {
 	// open repository
 	r, err := git.PlainOpenWithOptions(path, &git.PlainOpenOptions{DetectDotGit: true})
 	if err != nil {
@@ -132,7 +132,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	next, err := nextVersion(`.`)
+	next, err := NextVersion(`.`)
 	if err != nil {
 		log.Fatal("couldn't get next version", zap.Error(err))
 	}
