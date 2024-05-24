193c5e420779287ab4a7a46ddcde8273104bb57b
Scott Leggett <scott@sl.id.au>
2024-05-24 21:56:31 +0800

    fix: git security workaround
    This fixes a security check in recent git versions:
    
      fatal: detected dubious ownership in repository at '/github/workspace'

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 set -eu
 # if the first argument to the script is "true", it will push a tag to the repository.
 WRITE_TAG="$1"
+# the runner workspace will be mounted here, and git complains otherwise
+git config --global --add safe.directory /github/workspace
 # if the ccv tag exists, just exit
 if [ "$(git tag -l "$(ccv)")" ]; then
 	echo "new_tag=false" >>"$GITHUB_OUTPUT"
